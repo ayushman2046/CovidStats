1201e058bec3c05e99786c1c4e99d373d39d10c6
Ayushman Singh <77488166+ayushman2046@users.noreply.github.com>
2022-06-12 00:52:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+$.ajax({
+    type : "get",
+    url : "http://api.covid19api.com/summary",
+    success : function(response){
+        console.log(response)
+        for(var i=0;i<response.Countries.length;i++){
+
+            var tablerow = `<tr>   <td>${response.Countries[i].Country}</td>  <td>${response.Countries[i].TotalConfirmed}</td>   <td>${response.Countries[i].TotalConfirmed-response.Countries[i].TotalRecovered}</td>  <td>${response.Countries[i].TotalRecovered}</td>   <td>${response.Countries[i].TotalDeaths}</td>/tr>`
+            $('#tbody').append(tablerow)
+
+        }
+
+        $('#covidtable').DataTable()
+    },
+    error : function(error){
+        console.log(error)
+    }
+})  
